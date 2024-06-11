a2c4c45cfd6912e5047afcdaa690e94d248e4197
AhmedIBRAHIM01 <103690053+AhmedIBRAHIM01@users.noreply.github.com>
2024-06-11 23:18:18 +0200

    add the model form other branch

---
+++ app/src/main/java/com/example/fussball_em_2024_app/model/MatchResult.kt
@@ -0,0 +1,13 @@
+package com.example.fussball_em_2024_app.model
+
+class MatchResult(
+    var resultID: Int,
+    var resultName: String,
+    var pointsTeam1: Int,
+    var pointsTeam2: Int,
+    var resultOrderID: Int,
+    var resultTypeID: Int,
+    var resultDescription: String
+) {
+
+}
