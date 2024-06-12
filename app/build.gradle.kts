19c4d9db16a1dd26938d7c98016ebf01af1aacb2
AhmedIBRAHIM01 <103690053+AhmedIBRAHIM01@users.noreply.github.com>
2024-06-12 14:51:58 +0200

    feat:the dependencies have been updated to the current version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,13 +52,13 @@ android {
 dependencies {
 
     //Compose ViewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.1")
 
     //Network calls
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
 
     //Json to Kotlin object mapping
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
     //Image loading
     implementation("io.coil-kt:coil-compose:2.4.0")
